c8e28cfb5aeefe8ad7f00736f034ea1b7e88533d
Fiindah <fiendah24@gmail.com>
2025-05-19 12:04:44 +0700

    tugas tiga for

--- lib/tugas_tiga.dart
+++ lib/tugas_tiga.dart
@@ -1,20 +1,19 @@
-// Soal 2 Mencetak Karakter Bintang 5x
-void cetakBintang() {
-  String bintang = ""; // deklarasi string kosong untuk menampung bintang
-  for (int i = 0; i < 5; i++) {
-    bintang += "*"; // Tambahkan satu bintang ke dalam string setiap iterasi
-
-  }
-  print(bintang); // Cetak seluruh string bintang dalam satu baris
-
-}
-
 // Soal 1 Menampilkan bilangan ganjil 1 sampai 20
 void bilanganGanjil() {
   // menampilkan bilangan ganjil
   for (int i = 1; i <= 20; i +=2 ) {
     print(i);
   }
+}
+
+// Soal 2 Mencetak Karakter Bintang 5x
+void cetakBintang() {
+  String bintang = ""; // deklarasi string kosong untuk menampung bintang
+  for (int i = 0; i < 5; i++) {
+    bintang += "*"; // Tambahkan satu bintang ke dalam string setiap iterasi
+  }
+  print(bintang); // Cetak seluruh string bintang dalam satu baris
+
 }
 
 // Soal 3 Mencetak Nama Berulang
@@ -39,7 +38,7 @@ void daftarBuah() {
 
 // Soal 5 Simulasi daftar belanja
 void daftarBelanja() {
-  List<String> daftarBelanja = ["Roti", "Susu", "Telur", "Keju"];
+  List<String> daftarBelanja = ["Beras", "Minyak", "Telur", "Keju"];
   int counter = 1;
 
   for (String item in daftarBelanja) {
